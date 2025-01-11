9fec0a5e27d742fb665ef464a1be386dfdb07be7
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-01-11 23:46:23 +0500

    Working

--- script.js
+++ script.js
@@ -1,50 +1,64 @@
-// var a = "nameless"
-// let b = "pls declare"
-// function loud(){
-//     if(true){
-//          a = "Ahmad";
-//          b = "Rana";
-//         // console.log(a, "this is var0")
-//         // console.log(b, "this is let0")
-//     }
-//     // console.log(a , "this is var")
-//     // console.log(b, "this is let")
-// }
+// // var a = "nameless"
+// // let b = "pls declare"
+// // function loud(){
+// //     if(true){
+// //          a = "Ahmad";
+// //          b = "Rana";
+// //         // console.log(a, "this is var0")
+// //         // console.log(b, "this is let0")
+// //     }
+// //     // console.log(a , "this is var")
+// //     // console.log(b, "this is let")
+// // }
 
-// console.log(a, "this is var1")
-// console.log(b, "this is let1")
+// // console.log(a, "this is var1")
+// // console.log(b, "this is let1")
 
 
-// loud()
+// // loud()
 
-// window.alert("working")
+// // window.alert("working")
 
-// for (let i = 0; i < 5; i++) {
-//     console.log(i);
-// }
+// // for (let i = 0; i < 5; i++) {
+// //     console.log(i);
+// // }
 
-// const saandInaam = {age: 26, name: "Hassan Inaam", job: "Customer Support", favouritePerson: "Rafia"};
-// console.log(saandInaam)
+// // const saandInaam = {age: 26, name: "Hassan Inaam", job: "Customer Support", favouritePerson: "Rafia"};
+// // console.log(saandInaam)
 
-// saandInaam.favouritePerson = "Nomi Dadra";
-// console.log(saandInaam)
+// // saandInaam.favouritePerson = "Nomi Dadra";
+// // console.log(saandInaam)
+
+
+// // console.log('Start');
+
+// // setTimeout(() => {
+// //     console.log('This is asynchronous!');
+// // }, 1000);
+
+// // console.log('End');
 
 
 // console.log('Start');
 
-// setTimeout(() => {
+// const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// delay(1000).then(() => {
 //     console.log('This is asynchronous!');
-// }, 1000);
+// });
 
 // console.log('End');
+                  
+// const abc = "Khan"
+// console.log(typeof abc)
 
+// let num1 = 100;
+// console.log(num1 += 100)
 
-console.log('Start');
+function check(){
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    const input = prompt("Enter anything you want...");
+    console.log(typeof input);
+}
 
-delay(1000).then(() => {
-    console.log('This is asynchronous!');
-});
-
-console.log('End');
+check()
