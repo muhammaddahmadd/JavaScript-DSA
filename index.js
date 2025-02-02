73c736d62994cdb931ead60e6fdf075f4e26514b
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-02-02 17:59:05 +0500

    Working on asynchronous

--- index.js
+++ index.js
@@ -1637,7 +1637,7 @@ function changeText(a){
 
 
 function getJson(url, errorMsg = "Something went wrong") {
-  return  fetch(url)
+  const x = fetch(url)
         .then((res) => {
             console.log(res)
             if (!res.ok) {
@@ -1646,6 +1646,7 @@ function getJson(url, errorMsg = "Something went wrong") {
 
             return res.json()
         })
+        return x;
 }
 
 
@@ -1657,7 +1658,7 @@ const fetched = function (country) {
                 changeText(data[0].name);
             }, 2000);
         })
-        .catch((err) => alert("Error: " + err));
+        .catch((err) => alert("Error: " + err.message));
 };
 
 
@@ -1667,3 +1668,139 @@ btn.addEventListener("click", function(){
 })
 
 
+
+
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}.
+The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=52.508&longitude=13.381. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+function GetFetchedData(url) {
+    return fetch(url)  // ✅ Pass the correct URL
+        .then(res => {
+            console.log(res);
+            if (!res.ok) {
+                throw new Error(`Please provide correct coordinates: ${res.status}`);
+            }
+            return res.json();
+        });
+}
+
+const whereAmI = function (lat, lng) {
+    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`;
+
+    GetFetchedData(url)  // ✅ Pass the correctly formatted URL
+        .then(data => {
+            console.log(`You are in ${data.city}, ${data.continent}`);
+        })
+        .catch(err => alert(err));
+};
+
+// ✅ Test cases
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+
+
+
+const lottery = new Promise((resolve, reject) => {
+    console.log("Lottery Draw Happening!!")
+    setTimeout(()=> {
+        if (Math.random() + 0.5 > 1) {
+            resolve("You won the lottery")
+        } else {
+            reject("You lost the lottery")
+        }
+    }, 2000)
+})
+
+lottery.then(res=> console.log(res)).catch(err => console.log(err))
+
+//promisifying 
+const waited = function(secs) {
+    return new Promise(resolve => {
+        setTimeout(resolve, secs * 1000)
+    })
+}
+
+waited(2).then(()=> {
+    console.log("I wiated for 2 sec")
+    return waited(5)
+}).then(res => console.log("i waited for 5 sec"))
+
+Promise.resolve("Abc").then(res=> console.log(res))
+
+Promise.reject(new Error("xyz")).catch(err => console.log(err))
+
+
+// function getMyLocation(){
+//  let v = navigator.geolocation.getCurrentPosition(position => {
+//     return position
+//   }, err =>{
+//     return err
+//   })
+//   return v;
+// }
+
+
+
+const getMyLocation = function(){
+    return new Promise((resolve, reject)=> {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+}
+
+
+
+
+// const getLoc = document.createElement("button");
+// getLoc.textContent = "Click me to get your location";
+// body.prepend(getLoc)
+// getLoc.addEventListener("click", getMyLocation)
+// getMyLocation().then(res=> console.log(res)).catch(err=> console.log(err))
+
+
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
